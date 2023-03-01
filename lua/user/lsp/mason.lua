030949fd6411f31f6bb673eb994c8cc3ad307843
fairulazmin <fairulazmin@yahoo.com>
2023-03-01 15:14:36 +0800

    add emmet

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -54,3 +54,20 @@ for _, server in pairs(servers) do
 
 	lspconfig[server].setup(opts)
 end
+
+local lspconfig = require('lspconfig')
+local configs = require('lspconfig/configs')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+  capabilities = capabilities,
+  filetypes = {'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less'},
+  init_options = {
+    html = {
+      options = {
+        ["bem.enabled"] = true,
+      }
+    }
+  }
+})
