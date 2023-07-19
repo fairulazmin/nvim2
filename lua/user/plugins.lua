befb3aafd64179abe42b8bcf6e1e8ba430e6fb1c
fairulazmin <fairulazmin@yahoo.com>
2023-07-19 10:17:33 +0800

    modify treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,7 +96,8 @@ return packer.startup(function(use)
 
   -- Treesitter
   use {
-    "nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter", 
+    run = ":TSUpdate"
   }
 
   -- Git
